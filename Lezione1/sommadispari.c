eddc0eccc64a8a1a297c8fb1e9a6eed4e8bdaaaa
Davide <d.rucci1@studenti.unipi.it>
2019-03-03 16:34:03 +0100

    Aggiunta Lezione1

--- Lezione1/sommadispari.c
+++ Lezione1/sommadispari.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int somma_dispari(int n) {
+    /* Caso Base */
     if(n == 1) {
         return 1;
     }
 
-    return (2 * n - 1) + somma_dispari(n-1);
+    /* L'n-esimo numero dispari è 2n - 1,
+     * ad esempio il secondo numero dispari è 3 = 2*2 - 1.
+     * Lo accumuliamo sfruttando le chiamate ricorsive.
+     */
+    return (2 * n - 1) + somma_dispari(n-1); // Caso ricorsivo
 }
 
 int main(void) {
